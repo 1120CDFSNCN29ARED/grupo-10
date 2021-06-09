6d944f0e4fde41f70f425f4ba314062a12944ab1
Jona-Medina <jonaa.medina99@gmail.com>
2021-06-09 04:20:06 -0300

    DM Arreglos

---
+++ src/database/02_db_data.sql
@@ -0,0 +1,29 @@
+insert into category (name) values ('Resmas');
+insert into category (name) values ('Equipamiento de Oficina');
+insert into category (name) values ('Merchandising');
+insert into category (name) values ('Libreria');
+
+insert into user_type (id, name) values ('1', 'Regular User');
+insert into user_type (id, name) values ('2', 'Admin');
+
+insert into products (id, name, description, product_img, price, category_id) values (1, 'Resma 500 hojas Color Blanco', 'Resma de 500 hojas tamaño A4, gramaje de 80g, color Blanco. Superficie super lisa para proporcionar buenas impresiones', 'ResmaBlanco.jpg', 30, 1);
+insert into products (id, name, description, product_img, price, category_id) values (2, 'Resma 500 hojas Color Amarillo', 'Resma de 500 hojas tamaño A4, gramaje de 80g, color Amarillo. Superficie super lisa para proporcionar buenas impresiones', 'ResmaAmarillo.jpg', 25, 1);
+insert into products (id, name, description, product_img, price, category_id) values (3, 'Resma 500 hojas Gramaje Fino', 'Resma de 500 hojas tamaño A4, gramaje de 70g, color Blanco. Superficie mas fina ideal para la escritura.', 'ResmaFina.jpg', 20, 1);
+insert into products (id, name, description, product_img, price, category_id) values (4, 'Resma 500 hojas Premium', 'Resma de 500 hojas tamaño A4, gramaje de 80g, color Blanco. Calidad superior al resto para mayor duracion.', 'ResmaPremium.jpg', 35, 1);
+insert into products (id, name, description, product_img, price, category_id) values (5, 'Resma 500 hojas Ecologica', 'Resma de 500 hojas tamaño A4, gramaje de 75g, color Blanco. Hoja hecha de materiales 100% reciclado.', 'ResmaEcologica.jpg', 25, 1);
+insert into products (id, name, description, product_img, price, category_id) values (6, 'Resma 500 hojas Varios Colores', 'Resma de 500 hojas tamaño A4, gramaje de 80g, varios colores. Resma compuesta con hojas de hasta 6 colores.', 'ResmaColores.jpg', 40, 1);
+
+insert into products (id, name, description, product_img, price, category_id) values (7, 'Caja 12 Resmas Color Blanco', 'Caja con 12 Resmas tamaño A4, gramaje de 80g, color Blanco. Superficie super lisa para proporcionar buenas impresiones', 'CajaBlanco.jpg', 330, 1);
+insert into products (id, name, description, product_img, price, category_id) values (8, 'Caja 12 Resmas Color Amarillo', 'Caja con 12 Resmas tamaño A4, gramaje de 80g, color Amarillo. Superficie super lisa para proporcionar buenas impresiones', 'CajaAmarillo.jpg', 275, 1);
+insert into products (id, name, description, product_img, price, category_id) values (9, 'Caja 12 Resmas Gramaje Fino', 'Caja con 12 Resmas tamaño A4, gramaje de 70g, color Blanco. Superficie mas fina ideal para la escritura.', 'CajaFina.jpg', 220, 1);
+insert into products (id, name, description, product_img, price, category_id) values (10, 'Caja 12 Resmas Premium', 'Caja con 12 Resmas tamaño A4, gramaje de 80g, color Blanco. Calidad superior al resto para mayor duracion.', 'CajaPremium.jpg', 385, 1);
+insert into products (id, name, description, product_img, price, category_id) values (11, 'Caja 12 Resmas Ecologica', 'Caja con 12 Resmas tamaño A4, gramaje de 75g, color Blanco. Hoja hecha de materiales 100% reciclado.', 'CajaEcologica.jpg', 275, 1);
+insert into products (id, name, description, product_img, price, category_id) values (12, 'Caja 12 Resmas Varios Colores', 'Caja con 12 Resmas tamaño A4, gramaje de 80g, varios colores. Resma compuesta con hojas de hasta 6 colores.', 'CajaColores.jpg', 440, 1);
+
+insert into products (id, name, description, product_img, price, category_id) values (13, 'Impresora', 'La forma simple de obtener lo esencial. Con la configuración impecable desde la PC e impresión confiable, usted puede satisfacer sus necesidades de impresión, escaneo y copia con una impresora asequible.', 'EqImpresora.jpg', 350, 2);
+insert into products (id, name, description, product_img, price, category_id) values (14, 'Fotocopiadora', 'Hacé todas las fotocopias que necesités con esta Fotocopiadora multifunción, siempre lista para facilitar tu rutina de trabajo o estudio.', 'EqFotoco.jpg', 550, 2);
+insert into products (id, name, description, product_img, price, category_id) values (15, 'PC para la Oficina', 'Esta PC Completa es un equipo específicamente diseñado para trabajos 24x7.', 'EqPc.jpg', 250, 2);
+insert into products (id, name, description, product_img, price, category_id) values (16, 'Proyector', 'Éste proyector tiene un diseño intuitivo, fácil de usar y un elegante chasis Negro. Posee una alta calidad de imagen para una excelente producción de imagen en casi cualquier entorno.', 'EqPc.jpg', 375, 2);
+insert into products (id, name, description, product_img, price, category_id) values (16, 'Destructora de Papeles', 'Destructora de papeles en tiras y particulas como cds y dvds, indispensable para oficinas e industrias donde se necesite achicar los residuos.', 'EqTrituradora.jpg', 295, 2);
+
+
